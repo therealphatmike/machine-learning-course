fa07abe9418b759e6e3bd10772d6d0ca3069f50d
T Michael Smith <leogoesgrr@gmail.com>
2021-02-02 06:54:19 -0600

    print train and test splits

--- src/regression/multiple_linear_regression/multiple_linear_regression.py
+++ src/regression/multiple_linear_regression/multiple_linear_regression.py
@@ -5,7 +5,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 
-data_set = pd.read_csv('50_Startups.csv')
+data_set = pd.read_csv('src/regression/multiple_linear_regression/50_Startups.csv')
 features = data_set.iloc[:, :-1].values
 dependent_vector = data_set.iloc[:, -1].values
 
@@ -13,5 +13,10 @@ ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remaind
 features = np.array(ct.fit_transform(features))
 
 feature_train, feature_test, dependent_train, dependent_test = train_test_split(
-  features, dependent_vector, test_size=0.2, random=0
+  features, dependent_vector, test_size=0.2, random_state=0
 )
+
+print("features training set: " + str(feature_train))
+print("dependent var training set: " + str(dependent_train))
+print("features test set: " + str(feature_test))
+print("dependent var test set: " + str(dependent_test))
