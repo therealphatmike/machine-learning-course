b3982b3de1d8d46c169406aa051453fe4bfad1c9
T Michael Smith <leogoesgrr@gmail.com>
2021-02-02 07:23:02 -0600

    training and predicting multiple linear regression

--- src/regression/multiple_linear_regression/multiple_linear_regression.py
+++ src/regression/multiple_linear_regression/multiple_linear_regression.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 data_set = pd.read_csv('src/regression/multiple_linear_regression/50_Startups.csv')
 features = data_set.iloc[:, :-1].values
@@ -20,3 +21,13 @@ print("features training set: " + str(feature_train))
 print("dependent var training set: " + str(dependent_train))
 print("features test set: " + str(feature_test))
 print("dependent var test set: " + str(dependent_test))
+
+regressor = LinearRegression()
+regressor.fit(feature_train, dependent_train)
+
+test_prediction = regressor.predict(feature_test)
+np.set_printoptions(precision=2)
+reshaped_test_pred = test_prediction.reshape(len(test_prediction), 1)
+reshaped_dependent_pred = dependent_test.reshape(len(dependent_test), 1)
+concatenated_sets = np.concatenate((reshaped_test_pred, reshaped_dependent_pred), 1)
+print("concatenated test prediction vs test acutal: " + str(concatenated_sets))
